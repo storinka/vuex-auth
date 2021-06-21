a8a636ac2dbfa893905e9f080a1755bdbdb0bc9b
Davyd Kohut <expexes@gmail.com>
2021-06-21 14:56:31 +0300

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+# Storinka Vuex Auth
+
+A Vuex module and Vue Router middleware for authorization.
+
+## Installation
+
+```shell
+yarn add @storinka/vuex-auth
+```
+
+## Usage
+
+```javascript
+// store/index.js
+
+import { createStore } from "vuex";
+import { createAuthStoreModule } from "@storinka/vuex-auth";
+
+export default createStore({
+    modules: {
+        auth: createAuthStoreModule({
+            signIn: credentials => fetch("auth/signIn", credentials),
+            signUp: data => fetch("auth/signUp", data),
+            getMe: () => fetch("auth/getMe"),
+            logout: () => fetch("auth/logout"),
+
+            tokenKey: "auth_token",
+            namespaced: true,
+        }),
+    }
+});
+```
+
+```javascript
+// App.vue
+
+import { initAuth, setupRouterGuard } from "@storinka/vuex-auth";
+
+export default {
+    beforeCreate() {
+        initAuth(this.$router, this.$store);
+        setupRouterGuard(this.$router, this.$store);
+    }
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,180 @@
+const defaultTokenKey = "auth_token";
+const defaultSignInRoute = "/login";
+const defaultMainRoute = "/";
+
+type CommitFn = (mutation: any, args?: any) => any;
+
+interface AuthResult {
+    user: any;
+    token: string;
+}
+
+interface AuthStoreConfig {
+    signIn: (credentials: any) => Promise<AuthResult>;
+    signUp: (data: any) => Promise<AuthResult>;
+    getMe: () => Promise<any>;
+    logout: () => Promise<any>;
+
+    storageTokenKey?: string;
+    signInRoute?: string;
+    mainRoute?: string;
+}
+
+interface AuthState {
+    isLoadingUser: boolean;
+    isLoggingOut: boolean;
+    isSigningIn: boolean;
+    isSigningUp: boolean;
+
+    user?: any;
+    token?: null | string;
+}
+
+export function createAuthStoreModule(config: AuthStoreConfig) {
+    if (!config.storageTokenKey) {
+        config.storageTokenKey = defaultTokenKey;
+    }
+    if (!config.signInRoute) {
+        config.signInRoute = defaultSignInRoute;
+    }
+    if (!config.mainRoute) {
+        config.mainRoute = defaultMainRoute;
+    }
+
+    return {
+        namespaced: true,
+
+        state: {
+            isLoadingUser: false,
+            isLoggingOut: false,
+            isSigningIn: false,
+            isSigningUp: false,
+
+            user: null,
+            token: localStorage.getItem(config.storageTokenKey),
+
+            config,
+        },
+        getters: {
+            isReady({ isLoadingUser }: AuthState) {
+                return !isLoadingUser;
+            },
+            isAuthorized({ user }: AuthState) {
+                return !!user;
+            }
+        },
+        mutations: {
+            login(state: AuthState, { user, token }: AuthResult) {
+                // @ts-ignore
+                localStorage.setItem(config.storageTokenKey, token);
+
+                state.user = user;
+                state.token = token;
+            },
+            logout(state: AuthState) {
+                state.user = null;
+                state.token = null;
+
+                // @ts-ignore
+                localStorage.removeItem(config.storageTokenKey);
+            }
+        },
+        actions: {
+            init({ commit, state }: { commit: CommitFn, state: AuthState }) {
+                if (state.token) {
+                    state.isLoadingUser = true;
+
+                    return config.getMe()
+                        .then(user => {
+                            commit("login", {
+                                user,
+                                token: state.token
+                            });
+
+                            state.isLoadingUser = false;
+
+                            return user;
+                        })
+                } else {
+                    return Promise.resolve();
+                }
+            },
+            signIn({ commit, state }: { commit: CommitFn, state: AuthState }, credentials: any) {
+                return config.signIn(credentials)
+                    .then(auth => {
+                        commit("login", auth);
+
+                        return auth;
+                    })
+                    .finally(() => {
+                        state.isSigningIn = false;
+                    });
+            },
+            signUp({ commit, state }: { commit: CommitFn, state: AuthState }, data: any) {
+                state.isSigningIn = true;
+
+                return config.signUp(data)
+                    .then(auth => {
+                        commit("login", auth);
+
+                        return auth;
+                    })
+                    .finally(() => {
+                        state.isSigningIn = false;
+                    });
+            },
+            logout({ commit, state }: { commit: CommitFn, state: AuthState }) {
+                state.isLoggingOut = true;
+
+                return config.logout()
+                    .then(result => {
+                        commit("logout");
+
+                        return result;
+                    })
+                    .finally(() => {
+                        state.isLoggingOut = false;
+                    });
+            }
+        }
+    };
+}
+
+export function setupRouterGuard(
+    $router: any,
+    $store: any
+) {
+    $router.beforeEach((to: any, from: any, next: any) => {
+        const { mainRoute, signInRoute } = $store.state.auth.config;
+
+        if ($store.getters["auth/isReady"]) {
+            if ($store.getters["auth/isReady"]) {
+                if (to.meta.guest) {
+                    return next(mainRoute);
+                }
+            } else {
+                if (!to.meta.guest) {
+                    return next(`/${signInRoute}?to=${encodeURI(to.path)}`);
+                }
+            }
+        }
+
+        return next();
+    });
+}
+
+export function initAuth(
+    $router: any,
+    $store: any
+) {
+    $store.dispatch("auth/init")
+        .then((user: any) => {
+            if (user) {
+                const { mainRoute } = $store.state.auth.config;
+
+                if ($router.currentRoute.meta.guest) {
+                    $router.replace(mainRoute)
+                }
+            }
+        })
+}
